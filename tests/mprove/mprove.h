be70a7724e025d71f6e3c34f46c93e10bb2af546
Saravanan Vijayakumaran <sarva.v@gmail.com>
2018-12-10 17:48:08 +0530

    Added MoneroExchange class and moved signature code to new file

--- tests/mprove/mprove.h
+++ tests/mprove/mprove.h
@@ -1,184 +1,163 @@
-#include "ringct/rctSigs.h"
-#include "cryptonote_basic/cryptonote_basic.h"
-#include "../io.h"
+#include "mproveSigs.h"
+#include <algorithm>
 
-struct lsagSig {
-  rct::keyV ss;
-  rct::key c;
-  rct::key I;
+struct mproveProof {
+  rct::keyV addrs;
+  rct::keyV cs;
+  rct::keyV cprimes;
+  std::vector<ringSig> gammas;
+  std::vector<lsagSig> sigmas;
+  rct::key msg;
+  rct::key cassets;
 };
 
-lsagSig LSAG_Gen(const rct::key &message, const rct::keyV & pk, const rct::key & x, const unsigned int index, hw::device &hwdev) {
-    lsagSig rv;
-    size_t cols = pk.size();
-    CHECK_AND_ASSERT_THROW_MES(cols >= 2, "Error! Why ring signature if cols = 1!");
-    CHECK_AND_ASSERT_THROW_MES(index < cols, "Index out of range");
-
-    size_t i = 0;
-    rct::key c, c_old, L, R, Hi, aHP;
-    sc_0(c_old.bytes);
-    rct::geDsmp Ip;
-    rct::key alpha;
-    rct::key aG;
-
-    rv.ss = rct::keyV(cols);
-    rct::keyV toHash(4);
-    toHash[0] = message;
-    toHash[1] = pk[index];
-    Hi = hashToPoint(pk[index]);
-    hwdev.mlsag_prepare(Hi, x, alpha, aG, aHP, rv.I);
-    toHash[2] = aG;
-    toHash[3] = aHP;
-    precomp(Ip.k, rv.I);
-
-    hwdev.mlsag_hash(toHash, c_old);
-
-    i = (index + 1) % cols;
-    if (i == 0) {
-        copy(rv.c, c_old);
-    }
-    while (i != index) {
-
-        rv.ss[i] = rct::skGen();            
-        sc_0(c.bytes);
-        addKeys2(L, rv.ss[i], c_old, pk[i]);
-        hashToPoint(Hi, pk[i]);
-        addKeys3(R, rv.ss[i], Hi, c_old, Ip.k);
-        toHash[1] = pk[i];
-        toHash[2] = L; 
-        toHash[3] = R;
-        hwdev.mlsag_hash(toHash, c);
-        copy(c_old, c);
-        i = (i + 1) % cols;
-        
-        if (i == 0) { 
-            copy(rv.c, c_old);
-        }   
-    }
-
-    sc_mulsub(rv.ss[index].bytes, c.bytes, x.bytes, alpha.bytes);
-    return rv;
-}
-
-bool LSAG_Ver(const rct::key &message, const rct::keyV & pk, const lsagSig & rv) {
-
-    size_t cols = pk.size();
-    CHECK_AND_ASSERT_MES(cols >= 2, false, "Error! Why ring signature if cols = 1!");
-    CHECK_AND_ASSERT_MES(rv.ss.size() == cols, false, "Bad rv.ss size");
-
-    for (size_t i = 0; i < rv.ss.size(); ++i)
-        CHECK_AND_ASSERT_MES(sc_check(rv.ss[i].bytes) == 0, false, "Bad ss slot");
-    CHECK_AND_ASSERT_MES(sc_check(rv.c.bytes) == 0, false, "Bad c");
-
-    size_t i = 0;
-    rct::key c,  L, R, Hi;
-    rct::key c_old = copy(rv.c);
-    rct::geDsmp Ip;
-    precomp(Ip.k, rv.I);
-
-    rct::keyV toHash(4);
-    toHash[0] = message;
-    i = 0;
-    while (i < cols) {
-        sc_0(c.bytes);
-        addKeys2(L, rv.ss[i], c_old, pk[i]);
-        hashToPoint(Hi, pk[i]);
-        CHECK_AND_ASSERT_MES(!(Hi == rct::identity()), false, "Data hashed to point at infinity");
-        addKeys3(R, rv.ss[i], Hi, c_old, Ip.k);
-        toHash[1] = pk[i];
-        toHash[2] = L; 
-        toHash[3] = R;
-        c = hash_to_scalar(toHash);
-        copy(c_old, c);
-        i = (i + 1);
-    }
-    sc_sub(c.bytes, c_old.bytes, rv.c.bytes);
-    return sc_isnonzero(c.bytes) == 0;  
-}
-
-struct ringSig {
-  rct::keyV ss;
-  rct::key c;
+class MoneroExchange
+{
+    size_t m_anonSetSize;
+    size_t m_ownkeysSetSize;
+    mproveProof m_proof;
+    rct::keyV m_ownKeys;
+    std::vector<rct::xmr_amount> m_ownAmounts;
+    rct::keyV m_amountMasks;
+    rct::xmr_amount m_maxAmount = 1000;
+    rct::keyV m_cprimeKeys;
+  public:
+    MoneroExchange(size_t anonSetSize, size_t ownkeysSetSize, rct::key message);
+    mproveProof GenerateProofOfAssets();
 };
 
-ringSig RingSig_Gen(const rct::key &message, const rct::keyV & pk, const rct::key & x, const unsigned int index) {
-    ringSig rv;
-    size_t cols = pk.size();
-    CHECK_AND_ASSERT_THROW_MES(cols >= 2, "Error! Why ring signature if cols = 1!");
-    CHECK_AND_ASSERT_THROW_MES(index < cols, "Index out of range");
+MoneroExchange::MoneroExchange(size_t anonSetSize, size_t ownkeysSetSize, rct::key message)
+{
+  m_anonSetSize = anonSetSize;
+  m_ownkeysSetSize = ownkeysSetSize;
+  m_proof.msg = message;
+  m_proof.addrs = rct::keyV(anonSetSize);
+  m_proof.cs = rct::keyV(anonSetSize);
+  m_ownKeys = rct::keyV(anonSetSize);
+  m_ownAmounts = std::vector<rct::xmr_amount>(anonSetSize);
+  m_amountMasks = rct::keyV(anonSetSize);
+  m_cprimeKeys = rct::keyV(anonSetSize);
 
-    size_t i = 0;
-    rct::key c, c_old, L;
-    sc_0(c_old.bytes);
-    rct::key alpha;
-    rct::key aG;
-
-    rv.ss = rct::keyV(cols);
-    rct::keyV toHash(cols+2);
-
-    for (i = 0 ; i < cols ; i++)
+  for (size_t i = 0; i < anonSetSize; i++)
+  {
+    if (i < ownkeysSetSize)
     {
-      toHash[i] = pk[i];
+      m_ownKeys[i] = rct::skGen();
     }
-    toHash[cols] = message;
-    rct::skpkGen(alpha, aG);
-    toHash[cols+1] = aG;
-
-    c_old = rct::hash_to_scalar(toHash);
-
-    i = (index + 1) % cols;
-    if (i == 0) {
-        copy(rv.c, c_old);
+    else
+    {
+      sc_0(m_ownKeys[i].bytes);
     }
-    while (i != index) {
+  }
 
-        rv.ss[i] = rct::skGen();            
-        sc_0(c.bytes);
-        addKeys2(L, rv.ss[i], c_old, pk[i]);
-        toHash[cols+1] = L;
-        c = hash_to_scalar(toHash);
-        copy(c_old, c);
-        i = (i + 1) % cols;
-        
-        if (i == 0) { 
-            copy(rv.c, c_old);
-        }   
+  // Distribute the known keys randomly in the anonymity set
+  std::random_shuffle(m_ownKeys.begin(), m_ownKeys.end());
+
+  for (size_t i = 0; i < anonSetSize; i++)
+  {
+    if (sc_isnonzero(m_ownKeys[i].bytes) == 0)
+    {
+      m_proof.addrs[i] = rct::scalarmultBase(m_ownKeys[i]);
+      m_ownAmounts[i] = rct::randXmrAmount(m_maxAmount);
+      m_amountMasks[i] = rct::skGen();
+      m_proof.cs[i] = rct::commit(m_ownAmounts[i], m_amountMasks[i]);
+      m_cprimeKeys[i] = rct::skGen();
+      m_proof.cprimes[i] = rct::scalarmultBase(m_cprimeKeys[i]);
     }
-
-    sc_mulsub(rv.ss[index].bytes, c.bytes, x.bytes, alpha.bytes);
-    return rv;
+    else
+    {
+      m_proof.addrs[i] = rct::pkGen();
+      m_proof.cs[i] = rct::pkGen();
+      m_ownAmounts[i] = 0;
+      m_cprimeKeys[i] = rct::skGen();
+      rct::addKeys(m_proof.cprimes[i], rct::scalarmultBase(m_cprimeKeys[i]), m_proof.cs[i]);
+    }
+  }
 }
 
-bool RingSig_Ver(const rct::key &message, const rct::keyV & pk, const ringSig & rv) {
-    size_t cols = pk.size();
-    CHECK_AND_ASSERT_MES(cols >= 2, false, "Error! Why ring signature if cols = 1!");
-    CHECK_AND_ASSERT_MES(rv.ss.size() == cols, false, "Bad rv.ss size");
+mproveProof MoneroExchange::GenerateProofOfAssets()
+{
+  rct::keyV ringPks(2);
+  rct::keyV lsagPks(2);
+  rct::key csum, cprimesum;
 
-    for (size_t i = 0; i < rv.ss.size(); ++i)
-        CHECK_AND_ASSERT_MES(sc_check(rv.ss[i].bytes) == 0, false, "Bad ss slot");
-    CHECK_AND_ASSERT_MES(sc_check(rv.c.bytes) == 0, false, "Bad c");
-
-    size_t i = 0;
-    rct::key c, L;
-    rct::key c_old = copy(rv.c);
-
-    rct::keyV toHash(cols+2);
-    for (i = 0 ; i < cols ; i++)
+  for (size_t i = 0; i < m_anonSetSize; i++)
+  {
+    if (sc_isnonzero(m_ownKeys[i].bytes) == 0)
     {
-      toHash[i] = pk[i];
-    }
-    toHash[cols] = message;
+      m_cprimeKeys[i] = rct::skGen();
+      m_proof.cprimes[i] = rct::scalarmultBase(m_cprimeKeys[i]);
 
-    i = 0;
-    while (i < cols) {
-        sc_0(c.bytes);
-        addKeys2(L, rv.ss[i], c_old, pk[i]);
-        toHash[cols+1] = L;
-        c = hash_to_scalar(toHash);
-        copy(c_old, c);
-        i = (i + 1);
+      // Ring signature generation
+      ringPks[0] = m_proof.cprimes[i];
+      rct::subKeys(ringPks[1], m_proof.cprimes[i], m_proof.cs[i]);
+      m_proof.gammas[i] = RingSig_Gen(m_proof.msg, ringPks, m_cprimeKeys[i], 0);
+
+      // Linkable ring signature generation
+      lsagPks[0] = m_proof.addrs[i];
+      lsagPks[1] = ringPks[1];
+      m_proof.sigmas[i] = LSAG_Gen(m_proof.msg, lsagPks, m_ownKeys[i], 0, hw::get_device("default"));
     }
-    sc_sub(c.bytes, c_old.bytes, rv.c.bytes);
-    return sc_isnonzero(c.bytes) == 0;  
+    else
+    {
+      m_cprimeKeys[i] = rct::skGen();
+      rct::addKeys(m_proof.cprimes[i], rct::scalarmultBase(m_cprimeKeys[i]), m_proof.cs[i]);
+
+      // Ring signature generation
+      ringPks[0] = m_proof.cprimes[i];
+      rct::subKeys(ringPks[1], m_proof.cprimes[i], m_proof.cs[i]);
+      m_proof.gammas[i] = RingSig_Gen(m_proof.msg, ringPks, m_cprimeKeys[i], 1);
+
+      // Linkable ring signature generation
+      lsagPks[0] = m_proof.addrs[i];
+      lsagPks[1] = ringPks[1];
+      m_proof.sigmas[i] = LSAG_Gen(m_proof.msg, lsagPks, m_cprimeKeys[i], 1, hw::get_device("default"));
+    }
+  }
+
+  csum = rct::addKeys(m_proof.cs);
+  cprimesum = rct::addKeys(m_proof.cprimes);
+  rct::subKeys(m_proof.cassets, csum, cprimesum);
+
+  return m_proof;
 }
 
+bool MProveProofPublicVerification(mproveProof proof)
+{
+  rct::key csum, cprimesum, cassets;
+
+  csum = rct::addKeys(proof.cs);
+  cprimesum = rct::addKeys(proof.cprimes);
+  rct::subKeys(cassets, csum, cprimesum);
+  if(!rct::equalKeys(cassets, proof.cassets))
+  {
+    std::cout << "Commitments to assets does not satisfy equation" << std::endl;
+    return false;
+  }
+
+  rct::keyV ringPks(2);
+  rct::keyV lsagPks(2);
+  size_t anonSetSize = proof.addrs.size();
+
+  for (size_t i = 0; i < anonSetSize; i++)
+  {
+      // Ring signature verification
+      ringPks[0] = proof.cprimes[i];
+      rct::subKeys(ringPks[1], proof.cprimes[i], proof.cs[i]);
+      if(!RingSig_Ver(proof.msg, ringPks, proof.gammas[i]))
+      {
+        std::cout << "Ring signature verification failed at location " << i+1 << std::endl;
+        return false;
+      }
+
+      // Linkable ring signature verification
+      lsagPks[0] = proof.addrs[i];
+      lsagPks[1] = ringPks[1];
+      if(!LSAG_Ver(proof.msg, lsagPks, proof.sigmas[i]))
+      {
+        std::cout << "Linkable ring signature verification failed at location " << i+1 << std::endl;
+        return false;
+      }
+  }
+  return true;
+}
