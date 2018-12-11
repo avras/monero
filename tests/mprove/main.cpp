515f2d11a1ede66876683df9e5ac1b4b0a174f30
Saravanan Vijayakumaran <sarva.v@gmail.com>
2018-12-11 15:22:55 +0530

    Added command line arguments for testing

--- tests/mprove/main.cpp
+++ tests/mprove/main.cpp
@@ -1,9 +1,20 @@
 #include "mprove.h"
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  MoneroExchange exch(1000, 300, rct::identity());
+  if (argc < 3)
+  {
+    cout << "Usage: mprove-tests anonSetSize ownKeysSetSize" << endl;
+    return 1;
+  }
+  int anonSetSize, ownKeysSetSize;
+  anonSetSize = atoi(argv[1]);
+  ownKeysSetSize = atoi(argv[2]);
+
+  rct::key msg = rct::pkGen(); // Random 32 byte message
+  MoneroExchange exch(anonSetSize, ownKeysSetSize, msg);
   mproveProof p = exch.GenerateProofOfAssets();
   MProveProofPublicVerification(p);
   return 0;
